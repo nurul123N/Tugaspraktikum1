0b0896c9759c11ac6be7c11d9ccd1dbe5dbf7fbb
nurul123N <116323617+nurul123N@users.noreply.github.com>
2022-10-20 19:19:39 -0700

    Add files via upload

---
+++ sbeng.cs
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+ <html> 
+ <head>
+ <title>Ordered List</title>
+ </head> 
+ <body> 
+ <h2>Unordered List with Disc Bullets</h2>
+ <ul style="list-style-type:disc;"> 
+ <li>Coffee</li> 
+ <li>Tea</li> 
+  <li>Milk</li> 
+   </ul> 
+    <ul style="list-style-type:circle;">
+     <li>Coffee</li> 
+     <li>Tea</li> 
+     <li>Milk</li> 
+     </ul> 
+     . <ul style="list-style-type:square;"> 
+     <li>Coffee</li> 
+     <li>Tea</li> 
+     <li>Milk</li> 
+     </ul> 
+     </body> 
+     </html>
